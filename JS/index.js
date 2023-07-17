d4118c5cf0f6f462bf905e9ec74ca9d3ab2689c1
Zohre Mohebian <107933502+zmohebian13@users.noreply.github.com>
2023-07-17 21:08:53 +0330

    scroll navbar

--- JS/index.js
+++ JS/index.js
@@ -24,3 +24,38 @@ window.addEventListener("scroll", function () {
     backToTopBtn.classList.remove("active");
   }
 });
+
+console.log(window.scrollY);
+window.addEventListener("scroll", function () {
+  const homeSection = document.querySelector("#home-navbar");
+  const aboutSection = document.querySelector("#about-navbar");
+  const resumeSection = document.querySelector("#resume-navbr");
+  const portfolioSection = document.querySelector("#portfolio-navbar");
+  const contactSection = document.querySelector("#contact-navbar");
+  if (window.scrollY < 500) {
+    homeSection.classList.add("active");
+    aboutSection.classList.remove("active");
+    resumeSection.classList.remove("active");
+    portfolioSection.classList.remove("active");
+  } else if (window.scrollY > 500 && window.scrollY < 1998) {
+    aboutSection.classList.add("active");
+    homeSection.classList.remove("active");
+    resumeSection.classList.remove("active");
+  } else if (window.scrollY > 1998 && window.scrollY < 2750) {
+    resumeSection.classList.add("active");
+    aboutSection.classList.remove("active");
+    portfolioSection.classList.remove("active");
+  } else if (window.scrollY > 2750 && window.scrollY < 4100) {
+    portfolioSection.classList.add("active");
+    homeSection.classList.remove("active");
+    aboutSection.classList.remove("active");
+    resumeSection.classList.remove("active");
+    contactSection.classList.remove("active");
+  } else if (window.scrollY > 4100 && window.scrollY < 4855) {
+    contactSection.classList.add("active");
+    portfolioSection.classList.remove("active");
+    homeSection.classList.remove("active");
+    aboutSection.classList.remove("active");
+    resumeSection.classList.remove("active");
+  }
+});
